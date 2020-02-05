5ff8ee34288daee3466cc26985e00880a64b8df8
Damely Tineo <damely.tineo07@gmail.com>
2020-02-05 15:45:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,9 +12,8 @@ class Student
   end
   
   def self.create_from_collection(students_array)
-    students_array.each {|student|
-      student  
-    }
+    students_array.each  do |key, value|
+      puts"#{key}: #{value}"
   end
 
   def add_student_attributes(attributes_hash)
